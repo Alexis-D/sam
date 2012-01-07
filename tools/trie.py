c77d12a95394b51c657f0a79a17e388ea940ea33
Alexis Daboville <alexis.daboville@gmail.com>
2012-01-07 15:57:42 +0000

    Fixed a huge bug (in __contains__)
    * Now use $ to mark the end of words
    * Should be unit tested asap

--- tools/trie.py
+++ tools/trie.py
@@ -15,21 +15,32 @@ class Trie:
         for w in iterable:
             self.insert(w)
 
-    def insert(self, word):
-        """Insert a single word (word) in the trie."""
+    def insert(self, word, first=True):
+        """Insert a single word (word) in the trie.
+
+        first shouldn't be used, it's an internal parameter
+        """
 
         if len(word):
+            if first: # used as a sentinel to mark the end of the words
+                      # otherwise words like "de" would be allowed if
+                      # "deterrer" was in the trie
+                word += '$'
+
             c, w = word[0], word[1:]
             if c in self.childs:
-                self.childs[c].insert(w)
+                self.childs[c].insert(w, False)
             else:
-                self.childs.setdefault(c, Trie()).insert(w)
+                self.childs.setdefault(c, Trie()).insert(w, False)
 
     def __contains__(self, word):
         """in keyword, return true if word is in the trie."""
 
         if len(word) == 0:
-            return True
+            return False
+
+        if len(word) == 1:
+            return word == '$'
 
         c, w = word[0], word[1:]
 
